14c8684604980c0b8e583637dbf67f603636b634
Pablo Gil <lakesis@gmail.com>
2012-07-04 10:16:50 +1000

    What I was sayin'

--- eventi.js
+++ eventi.js
@@ -20,11 +20,12 @@ var Eventi = (function(Eventi, window, document, undefined){
 		'select':'input','change':'input',
 		'submit':'form','reset':'form',
 		'error':'img','load':'img','abort':'img'
-	}
+	},
+	currentHandler
 	;
 
 	// http://perfectionkills.com/detecting-event-support-without-browser-sniffing/
-	function isEventSupported(eventName) {
+	var isEventSupported = function(eventName) {
 		var el = document.createElement(TAGNAMES[eventName] || 'div');
 		eventName = 'on' + eventName;
 		var isSupported = (eventName in el);
@@ -57,69 +58,53 @@ var Eventi = (function(Eventi, window, document, undefined){
 	};
 	
 	// FIRING METHOD
+	
+	var fireWrapper = function(event, data) {
+		if('addEventListener' in document.documentElement){		// Event handler execution wrapping (Non IE)
+			document.addEventListener('eventWrapper', function(e){ 
+				currentHandler.call(event.currentTarget, event, data);
+				document.removeEventListener('eventWrapper',arguments.callee, false);
+			});		
+			var e =  document.createEvent('Event');
+			e.initEvent('eventWrapper', false, false);
+			document.dispatchEvent(e);
+		} else{													// Event handler execution wrapping (IE)
+			document.documentElement.eventWrapper = 0; 
+			document.documentElement.attachEvent("onpropertychange", function(e) {
+				if (e.propertyName == "eventWrapper") {
+					currentHandler.call(event.currentTarget, event, data);
+					document.documentElement.detachEvent("onpropertychange",arguments.callee);
+				}
+			});
+			document.documentElement.eventWrapper++;
+		}
+	};
+	
 	Eventi.fire = function(event, element, data){
 		if (typeof event == 'string'){
 			event = {type: event};
 			event.target =  event.srcElement = element || this;
 			event.currentTarget = event.target;
 			event.bubbling = true;
-			event.preventDefault = function(){}; // TODO
+			event.preventDefault = function(){}; // TODO?
 			event.stopPropagation = function(){ this.bubbling = false}; // Turn into a class?
 		} 
-		
-	/*	if(isEventSupported(event.type)){
-			// DOM events
-			if ('dispatchEvent' in document.documentElement){	// Non IE
-				var e =  document.createEvent('Event');
-				e.initEvent(event.type, true, true);
-				if(typeof element != 'undefined') element.dispatchEvent(e);
-				else document.dispatchEvent(e);
-			} else {											// IE
-				var e =  document.createEventObject('Event');
-				if(typeof element != 'undefined') element.fireEvent('on'+event.type, e);
-				else document.fireEvent('on'+event.type, e);
-			}
-		}else{*/
-			// Custom events
-			var thisEventListeners = listeners[event.type],
-			currentHandler
-			;
-			var fireWrapper = function() {
-				if('addEventListener' in document.documentElement){		// Event handler execution wrapping (Non IE)
-					document.addEventListener('eventWrapper', function(e){ 
-						currentHandler.call(event.currentTarget, event, data);
-						document.removeEventListener('eventWrapper',arguments.callee, false);
-					});		
-					var e =  document.createEvent('Event');
-					e.initEvent('eventWrapper', false, false);
-					document.dispatchEvent(e);
-				} else{													// Event handler execution wrapping (IE)
-					document.documentElement.eventWrapper = 0; 
-					document.documentElement.attachEvent("onpropertychange", function(e) {
-						if (e.propertyName == "eventWrapper") {
-							currentHandler.call(event.currentTarget, event, data);
-							document.documentElement.detachEvent("onpropertychange",arguments.callee);
-						}
-					});
-					document.documentElement.eventWrapper++;
-				}
-			};
-			for (var i=0, length = thisEventListeners.length; i < length; i++){	// Handler iteration 
-				if (typeof element != 'undefined'){
-					if (thisEventListeners[i].element === event.currentTarget){
-						currentHandler = thisEventListeners[i].listener;
-						fireWrapper();
-					} 
-					if (element.parentNode !== null && event.bubbling){ // Event bubbling 
-						event.currentTarget = element.parentNode;
-						Eventi.fire(event, element.parentNode, data);	
-					}
-				} else {
+		var thisEventListeners = listeners[event.type];
+		for (var i=0, length = thisEventListeners.length; i < length; i++){	// Handler iteration 
+			if (typeof element != 'undefined'){
+				if (thisEventListeners[i].element === event.currentTarget){
 					currentHandler = thisEventListeners[i].listener;
-					fireWrapper();
+					fireWrapper(event, data);
+				} 
+				if (element.parentNode !== null && event.bubbling){ // Event bubbling 
+					event.currentTarget = element.parentNode;
+					Eventi.fire(event, element.parentNode, data);	
 				}
+			} else {
+				currentHandler = thisEventListeners[i].listener;
+				fireWrapper(event, data);
 			}
-		//}	
+		}
 	};
 	
 	// REMOVING METHOD
